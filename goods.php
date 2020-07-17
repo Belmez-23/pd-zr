960a3ce52e005e8d9769b82c09f2020d1f1a6da7
marovudam <62387242+marovudam@users.noreply.github.com>
2020-07-17 16:34:32 +0300

    отзывы!

--- goods.php
+++ goods.php
@@ -78,8 +78,7 @@ echo '
             }
             echo '</nav>';
 
-        echo '<div id="Blok" >'; 
-        //вооот здесь было начало дива который в диве, но я его удалила кек маша небей
+        echo '<div id="Blok" >';
         $k = 0;
         $state1 = $pdo->query('SELECT * FROM `Вид` WHERE `кодТовара` = '.$_GET['red_id']);
         while($row1 = $state1->fetch(PDO::FETCH_ASSOC))
@@ -117,7 +116,88 @@ echo '
             {
                 echo '<tr><td width="30%">'.$row4['имяХарактеристики'].'</td><td>'.$row4['величина'].' '.$row4['едИзмерения'].'</td></tr>';
             }
-            echo '</table></div><h2>Все виды товара:</h2>';
+                        $state6 = $pdo->query('SELECT * FROM `Отзывы`, `Вид` WHERE `Вид`.`кодВида` = `Отзывы`.`кодВида` AND `Вид`.`кодТовара` = '.$_GET['red_id']);
+            echo "</table><h2>Отзывы</h2><br>";
+            $i = 0;
+            while ($row6 = $state6->fetch(PDO::FETCH_ASSOC))
+                $i++;
+            if ($i == 0)
+                echo "Отзывов пока нет. Напишите первый!<br>";
+            $state6 = $pdo->query('SELECT * FROM `Отзывы`, `Вид` WHERE `Вид`.`кодВида` = `Отзывы`.`кодВида` AND `Вид`.`кодТовара` = '.$_GET['red_id']);
+            while ($row6 = $state6->fetch(PDO::FETCH_ASSOC))
+            {
+                echo "<hr>";
+                $state7 = $pdo->query('SELECT * FROM `Пользователь` WHERE `кодПользователя` = '.$row6['кодПользователя']);
+                $row7 = $state7->fetch(PDO::FETCH_ASSOC);
+                echo $row7['имя']."<br>";
+                $i = $row6['общаяОценка'];
+                $k = 1;
+                echo '<div class="rating-result" width="100%">';
+                while ($k <= 5)
+                {
+                    if ($i >= $k)
+                        echo '<span class="active"></span>';
+                    else
+                        echo "<span></span>";
+                    $k++;
+                }
+                echo '</div><br>'.$row6['датаОтзыва']."<br>";
+                echo $row6['отзыв'];
+            }
+            
+            $state7 = $pdo->query('SELECT * FROM `Пользователь` WHERE `слкод` = "'.$_GET['user'].'"');
+            $row7 = $state7->fetch(PDO::FETCH_ASSOC);
+            if ($row7['e-mail'] == "-")
+            echo "<br> Вам необходимо <a href='reg.php?user=".$_GET['user']."'>зарегистрироваться</a> или <a href='user.php?user=".$_GET['user']."'>войти</a>, чтобы оставить отзыв<br>";
+            else {
+echo '
+<form name="a" method="get" action="add/add_comment.php">
+<h2>Отправить отзыв</h2>
+<input type="hidden" name="us" value='.$_GET['user'].'>
+<div class="rating-area" width="100%">
+        <input type="radio" id="star-5" name="rating" value="5">
+        <label for="star-5" title="Оценка «5»"></label>	
+        <input type="radio" id="star-4" name="rating" value="4">
+        <label for="star-4" title="Оценка «4»"></label>    
+        <input type="radio" id="star-3" name="rating" value="3">
+        <label for="star-3" title="Оценка «3»"></label>  
+        <input type="radio" id="star-2" name="rating" value="2">
+        <label for="star-2" title="Оценка «2»"></label>    
+        <input type="radio" id="star-1" name="rating" value="1">
+        <label for="star-1" title="Оценка «1»"></label>
+    </div>
+<br>
+<br>
+<select name="select" style="
+    width: 99%;
+    color: #f7daf7;
+    border-color: #f7daf7;
+    background-color: #5e216d;
+    border-radius: 8px;
+">
+<option disabled>Выберите вид товара</option>';
+$state8 = $pdo->query('SELECT * FROM `Вид` WHERE `кодТовара` = "'.$_GET['red_id'].'"');
+$row8 = $state8->fetch(PDO::FETCH_ASSOC);
+while ($row8 = $state8->fetch(PDO::FETCH_ASSOC))
+{
+    $state9 = $pdo->query('SELECT * FROM `Характеристики` WHERE `кодВида` = "'.$row8['кодВида'].'"');
+    $row9 = $state9->fetch(PDO::FETCH_ASSOC);
+    echo '<option value='.$row8['кодВида'].'>'.$row9['имяХарактеристики'].": ".$row9['величина']." ".$row9["едИзмерения"].'</option>';
+}
+echo '</select>
+<br>
+
+                <textarea maxlength="255" placeholder="Введите текст отзыва" name="area" style="
+    width: 98%;
+    border-color: #5e216d;
+    border-radius: 8px;
+"></textarea>
+            
+                <button id="butt2" type="submit">Оставить отзыв</button>
+              </div>
+            </form>';
+            }
+            echo '</div><h2>Все виды товара:</h2>';
             while ($row1 = $state1->fetch(PDO::FETCH_ASSOC))
             {
                 $state4 = $pdo->query('SELECT * FROM `Характеристики` WHERE `кодВида` = '.$row1['кодВида']);
@@ -168,6 +248,43 @@ echo '
             {
                 echo '<tr><td width="30%">'.$row4['имяХарактеристики'].'</td><td>'.$row4['величина'].' '.$row4['едИзмерения'].'</td></tr>';
             }
+            
+            
+            
+            $state6 = $pdo->query('SELECT * FROM `Отзывы`, `Вид` WHERE `Вид`.`кодВида` = `Отзывы`.`кодВида` AND `Вид`.`кодТовара` = '.$_GET['red_id']);
+            echo "<h2>Отзывы</h2>";
+            $i = 0;
+            while ($row6 = $state6->fetch(PDO::FETCH_ASSOC))
+                $i++;
+            if ($i == 0)
+                echo "Отзывов пока нет. Напишите первый!<br>";
+            $state6 = $pdo->query('SELECT * FROM `Отзывы`, `Вид` WHERE `Вид`.`кодВида` = `Отзывы`.`кодВида` AND `Вид`.`кодТовара` = '.$_GET['red_id']);
+            while ($row6 = $state6->fetch(PDO::FETCH_ASSOC))
+            {
+                echo "<hr>";
+                $state7 = $pdo->query('SELECT * FROM `Пользователь` WHERE `кодПользователя` = '.$row6['кодПользователя']);
+                $row7 = $state7->fetch(PDO::FETCH_ASSOC);
+                echo $row7['имя']."<br>";
+                $i = $row6['общаяОценка'];
+                while ($i > 0)
+                {
+                    $i--;
+                    echo '★';
+                }
+                echo '<br>'.$row6['датаОтзыва']."<br>";
+                echo $row6['отзыв'];
+            }
+            $state7 = $pdo->query('SELECT * FROM `Пользователь` WHERE `кодПользователя` = '.$row6['кодПользователя']);
+            $row7 = $state7->fetch(PDO::FETCH_ASSOC);
+            if ($row7['email'] == "-")
+            echo "<br> Вам необходимо зарегистрироваться, чтобы оставить отзыв<br>";
+            else {
+                
+            }
+            
+            
+            
+            
             $row2 = $state2->fetch(PDO::FETCH_ASSOC);
             if ($row1['остаток'] >0){
             echo '</table><br>В наличии: '.$row1['остаток'].'<br>
